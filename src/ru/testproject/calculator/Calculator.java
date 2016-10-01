af1a22f0e4ae6bc35bbec2f7ab55e87087d62537
Yukon85 <yurev.ks@gmail.com>
2016-10-01 18:23:36 +0300

    First version

---
+++ src/ru/testproject/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package ru.testproject.calculator;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int subtraction(int reducing, int subtrahend) {
+        int result = reducing - subtrahend;
+        return result;
+    }
+
+
+}
---
+++ src/ru/testproject/calculator/Main.java
@@ -0,0 +1,12 @@
+package ru.testproject.calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator owrCalculator = new Calculator();
+
+        ManWithCalculator manWithCalculator = new ManWithCalculator(owrCalculator);
+
+        int result = manWithCalculator.calculateSum(3, 5);
+        System.out.println(result);
+    }
+}
