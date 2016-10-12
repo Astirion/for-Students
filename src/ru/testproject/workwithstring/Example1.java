d54a174aea8a1a1b13df35847b70c2d624327f8d
Yukon85 <yurev.ks@gmail.com>
2016-10-12 22:04:48 +0300

    added example with Scanner

---
+++ src/ru/testproject/workwithstring/Example1.java
@@ -0,0 +1,13 @@
+package ru.testproject.workwithstring;
+
+import java.util.Scanner;
+
+public class Example1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        System.out.println("Это было введено: " + input);
+    }
+}
