fe7518ed97d1d974793d698200a97f9ebca58065
AndreyKirshin <volk-nora@list.ru>
2016-10-12 19:56:04 +0400

    the diagonal

--- src/ru/testproject/tasks/Loop4.java
+++ src/ru/testproject/tasks/Loop4.java
@@ -3,21 +3,53 @@ package ru.testproject.tasks;
 /**
  * Created by 1 on 08.10.2016.
  */
-public class Loop3 {
+public class Loop4 {
     public static void main(String[] args) {
         int a[][] = new int[10][10];
 
+        int m = 0;
+
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+
+                if (i > j) {
+                    m = 1;
+                } else {
+                    m = 0;
+                }
+                a[i][j] = m;
+
+            }
+        }
         ShowMeMatrix(a);
-    }
+        System.out.println("=========================");
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+
+                if (i < 10 - j) {
+                    m = 1;
+                } else {
+                    m = 0;
+                }
+                a[i][j] = m;
+
+            }
+
+        }
+        ShowMeMatrix(a);
+        System.out.println("=========================");
+
+            }
 
     public static void ShowMeMatrix(int[][] a) {
-        for (int j = 0; j < a.length; j++)
-        {
+        for (int j = 0; j < a.length; j++) {
             for (int i = 0; i < a[j].length; i++) {
                 System.out.print(a[j][i] + " ");
             }
             System.out.println();
         }
 
-        }
+    }
 }
