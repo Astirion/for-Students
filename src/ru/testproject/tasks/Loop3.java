a7b73030b0dd7652c13086bffa38b6515df30639
AndreyKirshin <volk-nora@list.ru>
2016-10-12 19:52:39 +0400

    this clss do matrix 10*10

--- src/ru/testproject/tasks/Loop3.java
+++ src/ru/testproject/tasks/Loop3.java
@@ -4,4 +4,20 @@ package ru.testproject.tasks;
  * Created by 1 on 08.10.2016.
  */
 public class Loop3 {
+    public static void main(String[] args) {
+        int a[][] = new int[10][10];
+
+        ShowMeMatrix(a);
+    }
+
+    public static void ShowMeMatrix(int[][] a) {
+        for (int j = 0; j < a.length; j++)
+        {
+            for (int i = 0; i < a[j].length; i++) {
+                System.out.print(a[j][i] + " ");
+            }
+            System.out.println();
+        }
+
+        }
 }
