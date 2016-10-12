3b907ef70083eca051593a83de57363ef607684d
AndreyKirshin <volk-nora@list.ru>
2016-10-12 19:54:42 +0400

    add mult & division methods

--- src/ru/testproject/calculator/Calculator.java
+++ src/ru/testproject/calculator/Calculator.java
@@ -12,6 +12,9 @@ public class Calculator {
     }
 
     public int multiplication(int a, int b) {
-        return 10;
+        return a * b;
+    }
+    public int division(int a, int b) {
+        return a / b;
     }
 }
