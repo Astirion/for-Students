89e07da3ec2e2b80e65657a20fd491fef73fe792
Yukon85 <yurev.ks@gmail.com>
2016-10-19 19:50:41 +0300

    added switch example

--- src/ru/testproject/workwithstring/Example1.java
+++ src/ru/testproject/workwithstring/Example1.java
@@ -6,8 +6,24 @@ public class Example1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String input = sc.nextLine();
+        System.out.println("Введите действие: ");
 
-        System.out.println("Это было введено: " + input);
+        int input = sc.nextInt();
+        String output;
+
+        switch (input) {
+            case 1: output = "Новая игра";
+                break;
+            case 2: output = "Сохранить игру";
+                break;
+            case 3: output = "Загрузить игру";
+                break;
+            case 4: output = "Выход";
+                break;
+            default: output = "Неверная команда";
+
+        }
+
+        System.out.println(output);
     }
 }
