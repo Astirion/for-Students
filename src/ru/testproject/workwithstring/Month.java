4810223b53905a96d98700dd3a78c9fdaca23da4
Jiger <jiger@bigmir.net>
2016-10-25 21:56:35 +0300

    simplified code month

--- src/ru/testproject/workwithstring/Month.java
+++ src/ru/testproject/workwithstring/Month.java
@@ -1,7 +1,43 @@
 package ru.testproject.workwithstring;
 
+import java.util.Scanner;
+
 /**
- * Created by Stanislav on 19.10.2016.
+ * Created by Jiger on 19.10.2016.
  */
 public class Month {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите номер месяца: ");
+
+        String month = sc.nextLine();
+        String output;
+
+        switch (month) {
+            case ("12"):
+            case ("1"):
+            case ("2"):
+                output = "Зима";
+                break;
+            case ("3"):
+            case ("4"):
+            case ("5"):
+                output = "Весна";
+                break;
+            case ("6"):
+            case ("7"):
+            case ("8"):
+                output = "Лето";
+                break;
+            case ("9"):
+            case ("10"):
+            case ("11"):
+                output = "Осень";
+                break;
+            default:
+                output = "Бухабрь";
+        }
+        System.out.print(output);
+    }
 }
