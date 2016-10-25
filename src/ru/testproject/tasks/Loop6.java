c5b8029936a9e6a90329144d60fab561acd8b561
Jiger <jiger@bigmir.net>
2016-10-25 23:10:53 +0300

    New Loop with *

--- src/ru/testproject/tasks/Loop6.java
+++ src/ru/testproject/tasks/Loop6.java
@@ -1,7 +1,21 @@
 package ru.testproject.tasks;
 
 /**
- * Created by Stanislav on 25.10.2016.
+ * Created by Jiger on 25.10.2016.
  */
 public class Loop6 {
+    public static void main(String[] args) {
+        int[][] a = new int[3][3];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (j < (a.length - i)) {
+                    a[i][j] = 1;
+                } else {
+                    a[i][j] = 0;
+                }
+                System.out.print(a[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
